1219371812f995b4b43fee53db63c491212ea7a9
Hemantika <hemarpatel@yahoo.co.uk>
2022-12-13 20:22:59 +0000

    second commit

---
+++ Javahomework3.java
@@ -0,0 +1,16 @@
+package Java_lerning1;
+
+public class Javahomework3
+{
+    public static void main(String args[])
+
+    {
+        System.out.println("Software Development Models");
+        System.out.println("Waterfall");
+        System.out.println("Iterative");
+        System.out.println("V-Model");
+        System.out.println("Verification and Validation");
+        System.out.println("Agile");
+        System.out.println("Definiton of Redy/Done");
+    }
+}
---
+++ Javahomework4.java
@@ -0,0 +1,15 @@
+package Java_lerning1;
+
+public class Javahomework4
+{
+    public static void main(String args[])
+
+{
+    System.out.println("What is TDD");
+    System.out.println("What is BDD");
+    System.out.println("BDD keywords");
+    System.out.println("How to write test cases with BDD");
+    System.out.println("Maintenance");
+}
+
+}
